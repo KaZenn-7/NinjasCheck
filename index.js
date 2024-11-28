e7c29c1d432c67605ef518a49253939839138c66
Ramo s <ramodev.s155@gmail.com>
2024-11-27 21:32:33 -0300

    finalizado - exportando todas as funcoes para ser utilizado como modulo em outro projeto

--- index.js
+++ index.js
@@ -1,38 +1,7 @@
 import { splitNinjas } from "./func/splitNinjas.js";
 import { countByVillage } from "./func/countByVillage.js"
 import { formatMessage } from "./func/formatMessage.js";
+import { findClan } from "./func/findClan.js"
+import { findPatente } from "./func/findPatente.js"
 
-let stringg = `🎊👑📛Shirohege D. Welk📛👑🎊
-🎊🀄®️Levi Raiunko®️🀄🎊
-🎊🀄🦈Izuna hoshigaki🦈🀄🎊
-🎊♦️🌘Gojo Urashiki🌘♦️🎊
-🎊🃏⛔Lilith Chinoike⛔🃏🎊
-🎊🃏☠️Nuvem Kaguya☠️🃏🎊
-🎊🔘🐝Notz Kamizuru🐝🔘🎊
-🎊🔘🛗Has Kazekage🛗🔘🎊
-🎊⭕Ⓜ️Kuroko MomochiⓂ️⭕🎊
-🎊⭕🌘Kimiko Urashiki🌘⭕🎊
-🎊💠🐝Zeni Kamizuru🐝💠🎊
-🎊💠☮️Zenit Kozuki☮️💠
-🎊✴️🕊️Satoro Gojo Bakurei🕊️✴️🎊
-🎊✴️☸️Hagoromo Oden☸️✴️🎊
-🎊✳️🈚Musashi Render🈚✳️🎊
-🎊✳️☸️Wsl Oden☸️✳️🎊
-🎊🈂️🐝 Tobirama Kamizuru 🐝🈂️🎊
-🎊🈂️🐝Morgana kamizuru 🐝🈂️🎊`
-
-
-let main = async () => {
-
-    let oi = await splitNinjas(stringg);
-
-    let results = await countByVillage(oi)
-
-    let msg = await formatMessage(results); 
-
-    console.log(msg);
-    
-
-}
-
-main();
+export { splitNinjas, countByVillage, formatMessage, findClan, findPatente}
